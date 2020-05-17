9cbed673ef3464c4e991dfb6133804d60fbed5f5
Yung Cher Ho <ycho527@hotmail.com>
2020-05-17 23:53:31 +0800

    Created Dashboard, Home and Registration

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Registration from "./auth/Registration";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Homepage</h1>
+        <Registration />
+      </div>
+    );
+  }
+}
